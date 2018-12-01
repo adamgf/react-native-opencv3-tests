c57c712eb4837dfe38ccaac0ff36161b2b880a7e
Adam Freeman <adamgf@gmail.com>
2018-11-30 21:21:26 -0800

    checking in basically working jsut for simple showing video

---
+++ CvFaceDetection/App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/adamgf/react-native-opencv3-tests
+ *
+ * @format
+ * @flow
+ * @author Adam Freeman --> adamgf@gmail.com
+ * @description ==> super simple video preview app for CvCamera
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {CvCamera} from 'react-native-opencv3';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    const { type } = 'back';
+    return (
+      <View
+        style={styles.preview}
+      >
+        <CvCamera
+          style={styles.preview}
+          type={type}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  preview: {
+    flex: 1
+  },
+});
