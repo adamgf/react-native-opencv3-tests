e580c57baa0f40069dce6a8dc333c8c716f67b41
Adam Freeman <adamgf@gmail.com>
2018-12-04 21:05:02 -0800

    CvInvoke now working as parent tag for CvCamera now just have to get stack of parent tags working to apply multiple effects

--- CvFaceDetection/App.js
+++ CvFaceDetection/App.js
@@ -9,54 +9,28 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, View, DeviceEventEmitter, Image} from 'react-native';
-import {CvCamera} from 'react-native-opencv3';
+import {StyleSheet, View} from 'react-native';
+import {CvCamera, CvInvoke} from 'react-native-opencv3';
 
+type Props = {};
 export default class App extends Component<Props> {
-  constructor(props) {
-    super(props)
-    this.state = { videoImageSource : '' }
-  }
-
-  componentDidMount = () => {
-    DeviceEventEmitter.addListener('onCameraFrame', this.onCameraFrame.bind(this));
-  }
-
-  onCameraFrame(e) {
-    //console.log('e.data is: ' + e.data)
-    this.setState({ videoImageSource : e.data })
-  }
-
   render() {
-    const resolveAssetSource = require('react-native/Libraries/Image/resolveAssetSource')
-    let videoImageData = resolveAssetSource(require('./images/loading.gif')).uri
-    if (this.state.videoImageSource.length > 0) {
-      //const prependFilename = Platform.OS === 'ios' ? '' : 'file://'
-      videoImageData = 'data:image/jpg;base64,' + this.state.videoImageSource;
-    }
-
     const { type } = 'back';
     return (
-      <View style={styles.preview}>
-        <Image style={styles.preview}
-            source={{ uri: `${videoImageData}` }}
-        />
-        <CvCamera
-          style={{ flex : 1, opacity : 0.1 }}
-          type={type}
-        />
-    </View>
+      <View style={styles.preview} >
+        <CvInvoke func='cv:rotate' params='90.0,CCW'>
+          <CvCamera
+            style={styles.preview}
+            type={type}
+          />
+        </CvInvoke>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   preview: {
-    flex : 1,
-    backgroundColor : '#00000000'
-  },
-  preview2: {
-    width: '100%',
-    height: '30%'
+    flex: 1
   },
 });
