ff2f51146d118df4087a7d35908c7198466c846f
Adam Freeman <adamgf@gmail.com>
2018-12-04 21:13:54 -0800

    looks like recursive CvInvokes is working so multiple effects can be performed nested now should be able to start implementing effects

--- CvFaceDetection/App.js
+++ CvFaceDetection/App.js
@@ -18,12 +18,16 @@ export default class App extends Component<Props> {
     const { type } = 'back';
     return (
       <View style={styles.preview} >
-        <CvInvoke func='cv:rotate' params='90.0,CCW'>
+        <CvInvoke func='cv:cvtColor' params='GRAYSCALE'>
+          <CvInvoke func='cv:flip' params='HORIZONTAL'>
+          <CvInvoke func='cv:rotate' params='90.0,CCW'>
           <CvCamera
             style={styles.preview}
             type={type}
           />
         </CvInvoke>
+      </CvInvoke>
+    </CvInvoke>
       </View>
     );
   }
