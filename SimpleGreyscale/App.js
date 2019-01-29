eb53aecb8b3f7df3fc8477ed1dd4de28942ae7c1
Adam Freeman <adamgf@gmail.com>
2019-01-28 20:23:50 -0800

    using CvInvoke successfully on CvImage to greyscale and rotate 180

--- SimpleGreyscale/App.js
+++ SimpleGreyscale/App.js
@@ -8,10 +8,10 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image} from 'react-native';
-import {RNCv, CvImage, CvInvoke, ColorConv} from 'react-native-opencv3';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import {CvImage, CvInvoke, ColorConv} from 'react-native-opencv3';
 
-export default class App extends Component {  
+export default class App extends Component {
   render() {
     const originalImagePath = './images/girl_wide_brim_hat.png'
 
@@ -22,11 +22,13 @@ export default class App extends Component {
           source={ require(`${originalImagePath}`) }
         />
         <Text style={styles.captions}>Original</Text>
-        <CvInvoke func='cvtColor' params={{"p1":"srcMat","p2":"dstMat","p3":ColorConv.COLOR_BGR2GRAY}}>
-          <CvImage
-            style={{width: 200, height: 200}}
-            source={ require(`${originalImagePath}`) }
-          />
+        <CvInvoke func='rotate' params={{"p1":"dstMat","p2":"dstMat","p3":1}}>
+          <CvInvoke func='cvtColor' params={{"p1":"srcMat","p2":"dstMat","p3":ColorConv.COLOR_BGR2GRAY}}>
+            <CvImage
+              style={{width: 200, height: 200}}
+              source={ require(`${originalImagePath}`) }
+            />
+          </CvInvoke>
         </CvInvoke>
         <Text style={styles.captions}>Greyscaled</Text>
       </View>
